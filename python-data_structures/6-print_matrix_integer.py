1b0079d1d6ad0600202247037e6b8a9909bbd3e3
SacariLuz <asacaricachi@gmail.com>
2023-10-03 11:58:11 -0700

    prints a matrix of integers

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def print_matrix_integer(matrix=[[]]):
+    if matrix is None:
+        print("")
+    else:
+        for n in range(len(matrix)):
+            for m in range(len(matrix[n])):
+                print("{:d}".format(matrix[n][m]), end="")
+                if m != len(matrix[n]) - 1:
+                    print(" ", end="")
+            print("")
