d631f3e0ed56bcf01f22f7b04f2d9c707fc699c1
SacariLuz <asacaricachi@gmail.com>
2023-10-06 16:14:49 -0700

    function that returns a key

---
+++ python-more_data_structures/10-best_score.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def best_score(a_dictionary):
+    max = 0
+    name = ''
+
+    if not a_dictionary:
+        return None
+
+    for k in a_dictionary.keys():
+        v = a_dictionary[k]
+        if v > max:
+            max = v
+            name = k
+    return name
