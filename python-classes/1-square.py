319ef7ceb8e23a2058065d2bfa46cd2e13cda7ab
SacariLuz <asacaricachi@gmail.com>
2023-10-08 18:27:10 -0700

    that defines a square

---
+++ python-classes/1-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+"""
+represent a clase Square
+"""
+
+
+class Square:
+    """
+    define a square
+    """
+
+    def __init__(self, size):
+        """
+        Inicialize a new Square size.
+
+        Args:
+            size (int): size of the square.
+        """
+        self.__size = size
