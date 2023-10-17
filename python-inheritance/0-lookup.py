008d1dcbff8dbeef9a94dd502bfdff4867f39920
root <asacaricachi@gmail.com>
2023-10-16 20:53:59 -0700

    function that returns the list

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -2,12 +2,11 @@
 
 """Define object attribute lookup function"""
 
+
 def lookup(obj):
     """
     Return a list of an object
-
-    Args: obj
-
-    Return: list
+    Args:obj
+    Return:list
     """
     return dir(obj)
