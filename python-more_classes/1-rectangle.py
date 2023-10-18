4338b8f61569fa0ac2508796d46bbac6067425e8
root <asacaricachi@gmail.com>
2023-10-18 10:29:19 -0700

    a class that defines a rectagle

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -1,35 +1,18 @@
 #!/usr/bin/python3
-
 """Defines class rectangle"""
 
+
 class Rectangle:
-
     """Represent a rectagle."""
-
     def __init__(self, width=0, height=0):
-        """ 
+        """
         Inicializes new rectagle
         Args:
         width (int):the new rectangle
         height (int):of the new rectangle
         """
-
-        self.width = width
         self.height = height
-
-    @property
-    def width(self):
-        """Get width of the rectangle"""
-        return self.__width
-
-    @width.setter
-    def width(self, value):
-        """return width"""
-        if not isinstance(value, int):
-            raise TypeError("width must be an integer")
-        if value < 0:
-            raise ValueError("width must be >= 0")
-        self.__width = value
+        self.width = width
 
     @property
     def height(self):
@@ -44,3 +27,17 @@ class Rectangle:
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
+
+    @property
+    def width(self):
+        """Get width of the rectangle"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """return width"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
