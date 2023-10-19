71dea73f1ede530b82734a3c96ed9904379ebbf9
root <asacaricachi@gmail.com>
2023-10-19 09:57:43 -0700

    function that reads a text file

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Defines read_file function"""
+
+def read_file(filename=""):
+    """print the contents of text file"""
+    with open (filename) as f:
+        txt = f.read()
+        print(txt, end="")
