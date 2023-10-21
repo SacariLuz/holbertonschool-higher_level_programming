d4b84d257e86d9f7354b194cf21863998f4870e4
root <asacaricachi@gmail.com>
2023-10-21 15:51:51 -0700

    that raises an Exception

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Defines a base geometry class"""
+
+
+class BaseGeometry:
+    """Represent base geometry."""
+    def area(self):
+        """function an Exception"""
+        raise Exception("area() is not implemented")
