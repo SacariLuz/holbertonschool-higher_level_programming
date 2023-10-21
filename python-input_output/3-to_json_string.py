caf46bf5fd9b77358d5e6b73e075371efed3d12c
root <asacaricachi@gmail.com>
2023-10-21 12:13:36 -0700

    function that returns the JSON

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 """Defines a string to json function"""
+import json
 
 
 def to_json_string(my_obj):
-    import json
-    """return the json"""
-    return json.dumps(my_obj)
+    return (json.dumps(my_obj))
