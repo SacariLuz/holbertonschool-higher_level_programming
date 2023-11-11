bf5df018bb6eaccbe940d2ccd961eab1a7605845
SacariLuz <asacaricachi@gmail.com>
2023-11-11 13:02:42 -0800

    update a class square

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -9,26 +9,14 @@ Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """Reprsentación de la clase"""
+    """Represent a square."""
+
     def __init__(self, size):
-        """Define the init method and initialize atributes
+        """Initialize a new square.
+
         Args:
-            size(int): ofSquare
+            size (int): The size of the new square.
         """
-        super().integer_validator("size", size)
+        self.integer_validator("size", size)
+        super().__init__(size, size)
         self.__size = size
-
-    def area(self):
-        """This function calculates the area of the square
-        Return:
-            int: size ** 2
-        """
-        return self.__size ** 2
-
-    def __str__(self):
-        """method returns a text string of the class
-
-        Return:
-            string: representation ifromal
-        """
-        return f"[Rectangle] {self.__size}/{self.__size}"
