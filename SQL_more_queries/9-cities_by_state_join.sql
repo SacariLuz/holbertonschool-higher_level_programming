4b476387b2a53679c8fb2eacafbaabc601131fbb
SacariLuz <asacaricachi@gmail.com>
2023-12-12 15:24:01 -0800

    cities by states

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,3 +1,3 @@
 -- Enumera todas las cities contenidas en database hbtn_0d_usa
 -- Los resultados debe ordenarse en ascendente por cities.id
-SELECT cities-id, cities.name, states.name FROM cities INNER JOIN states ON cities.state_id = states.id;
+SELECT cities.id, cities.name, states.name FROM cities AS cities INNER JOIN states ON cities.state_id = states.id ORDER BY cities.id;
