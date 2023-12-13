20d69973e4bb253f86098755d597a8d369482242
SacariLuz <asacaricachi@gmail.com>
2023-12-12 20:52:21 -0800

    genre ID by show

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,7 +1,3 @@
 -- Lista todos los programas contenidos en hbtn_0d_tvshows
 -- Resultados clasificarse en orden ascendente por tv_shows.title y tv_show_genres.genre_id
-SELECT s.`title`, g.`genre_id`
-  FROM `tv_shows` AS s
-        INNER JOIN `tv_show_genres` AS g
-	ON s.`id` = g.`show_id`
- ORDER BY s.`title`, g.`genre_id`;
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows AS tv_shows INNER JOIN tv_show_genres AS tv_show_genres ON tv_shows.id = tv_show_genres.show_id GROUP BY tv_shows.title, tv_show_genres.genre_id;
