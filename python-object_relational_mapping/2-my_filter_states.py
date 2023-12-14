0e1d0f7fa2c5bbbcc0851492aaeafd912089d4c2
SacariLuz <asacaricachi@gmail.com>
2023-12-14 12:23:17 -0800

    filtrar estados por entrada de usuario

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
 """ scrip que filtra los valores en la tabla de estados de hbtn_0e_0_usa"""
 
-import sys
 import MySQLdb
+import sys
 
 if __name__ == "__main__":
 
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    c = db.cursor()
-    c.execute("SELECT * \
+    db = MySQLdb.connect(
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3]
+            )
+    cur = db.cursor()
+    cur.execute("SELECT * \
                  FROM `states` \
                 WHERE BINARY `name` = '{}'".format(sys.argv[4]))
-    [print(state) for state in c.fetchall()]
+    [print(state) for state in cur.fetchall()]
