511a035845103518f814e29663741a0f3ce869f3
SacariLuz <asacaricachi@gmail.com>
2023-12-14 12:19:54 -0800

    filtrar estados por entrada de usuario

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,26 +1,14 @@
 #!/usr/bin/python3
 """ scrip que filtra los valores en la tabla de estados de hbtn_0e_0_usa"""
 
+    import sys
+import MySQLdb
+
 if __name__ == "__main__":
-    import MySQLdb
-    from sys import argv
 
-    username = argv[1]
-    password = argv[2]
-    dbname = argv[3]
-    sname = argv[4]
-
-    database = MySQLdb.connect(host="localhost",
-                               port=3306,
-                               user=username,
-                               password=password,
-                               db=dbname)
-    cur = database.cursor()
-    cur.execute("SELECT * FROM states WHERE name \
-        LIKE BINARY '{}' ORDER BY id ASC")
-    rows = cur.fetchall()
-
-    for row in rows:
-        print("{}".format(row))
-    cur.close()
-    database.close()
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    c.execute("SELECT * \
+                 FROM `states` \
+                WHERE BINARY `name` = '{}'".format(sys.argv[4]))
+    [print(state) for state in c.fetchall()]
