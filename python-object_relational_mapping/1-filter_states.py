6da374a036d0c00d739168b073945949d616aad0
SacariLuz <asacaricachi@gmail.com>
2023-12-14 09:13:43 -0800

    estados de fistro

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,11 +1,30 @@
 #!/usr/bin/python3
 """Listado de todos los estados con N"""
 
-import sys
+from sys import argv
 import MySQLdb
 
+def main():
+    """
+    """
+
+    username = argv[1]
+    password = argv[2]
+    dbname = argv[3]
+
+    database = MySQLdb.connect(host="localhost",
+                               port=3306,
+                               user=username,
+                               password=password,
+                               db=dbname)
+    cur = database.cursor()
+    cur.execute("SELECT * FROM states WHERE name \ LIKE BINARY 'N%' ORDER BY id ASC")
+    rows = cur.fetchall()
+
+    for row in rows:
+        print("{}".format(row))
+    cur.close()
+    database.close()
+
 if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    c = db.cursor()
-    c.execute("SELECT * FROM `states` ORDER BY `id`")
-    [print(state) for state in c.fetchall() if state[1][0] == "N"]
+    main()
