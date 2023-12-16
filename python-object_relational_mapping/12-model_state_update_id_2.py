8f20f73cb21c69f6ce25c64c4274428cfdc6c4f3
SacariLuz <asacaricachi@gmail.com>
2023-12-15 23:20:47 -0800

    actualizado de estado

---
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Script que cambia el nombre obejto de estado de la database hbtn_0e_6_usa
+"""
+
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+        argv[1],
+        argv[2],
+        argv[3]),
+        pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    states = session.query(State).filter(State.id.contains("2")).first()
+    states.name = "New Mexico"
+
+    session.flush()
+    session.commit()
+    session.close()
