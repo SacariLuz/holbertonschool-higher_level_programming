2a04cc2a27f9763b68b31bd9a9f2440a1db45770
SacariLuz <asacaricachi@gmail.com>
2023-12-15 22:34:28 -0800

    lista los objetos de estado con a

---
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Scrip que enumere todos los objetos de estado
+que tiene la letra A, de la databas e hbtn_0e_6_usa
+"""
+
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == '__main__':
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    states = session.query(State).filter(
+        State.name.contains('a')).order_by(State.id.asc())
+
+    for state in states:
+        print("{}: {}".format(state.id, state.name))
+
+    session.close()
