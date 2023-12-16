32e24adc3716f0a18efae1050962bb9a11deb285
SacariLuz <asacaricachi@gmail.com>
2023-12-15 21:58:54 -0800

    primer estado

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -3,21 +3,23 @@
 Script que imprima el primer obejto estado de la database hbtn_0e_6_usa
 """
 
-import sys
+from sys import argv
+from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import State
 
-if __name__ == "__main__":
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}".format(sys.argv[1],
-                sys.argv[2],
-                sys.argv[3]),
-            pool_pre_ping=True)
+
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
     Session = sessionmaker(bind=engine)
     session = Session()
+    state_name = session.query(State).first()
 
-    state = session.query(State).order_by(State.id).first()
-    if state is None: 
-        print("Nothing")
+    if state_name is not None:
+        print("{}: {}".format(state_name.id, state_name.name))
     else:
-        print("{}: {}".format(state.id, state.name))
+        print("Nothing")
+    session.close()
