5c6e6eaf886943ac50569324bc5cc17200014d2a
SacariLuz <asacaricachi@gmail.com>
2023-12-15 22:36:08 -0800

    lista los objetos de estado con a

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -9,9 +9,13 @@ from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-if __name__ == '__main__':
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".format(
-        argv[1], argv[2], argv[3]), pool_pre_ping=True)
+if __name__ == "__main__":
+    engine = create_engine(
+            "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+        argv[1],
+        argv[2],
+        argv[3]),
+            pool_pre_ping=True)
     Base.metadata.create_all(engine)
 
     Session = sessionmaker(bind=engine)
@@ -22,5 +26,4 @@ if __name__ == '__main__':
 
     for state in states:
         print("{}: {}".format(state.id, state.name))
-
     session.close()
