7f75e02590f1f7368d02b72f03866b5ab74c3941
SacariLuz <asacaricachi@gmail.com>
2023-12-15 23:09:59 -0800

    añadido un nuevo estado

---
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+Script que agrega el objeto estado “Louisiana”
+en la datagbase hbtn_0e_6_usa
+"""
+
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        argv[1],
+        argv[2],
+        argv[3]
+        ),
+        pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    new_state = State(name="Louisiana")
+
+    session.add(new_state)
+    session.flush()
+    session.commit()
+
+    state = session.query(State).order_by(State.id.desc()).first()
+    print(state.id)
+    session.close()
