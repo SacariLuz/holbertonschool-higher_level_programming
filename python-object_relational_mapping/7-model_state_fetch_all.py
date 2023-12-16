a87e264c6b57b62c4d28bbfca35f9e8917ed88de
SacariLuz <asacaricachi@gmail.com>
2023-12-15 22:11:59 -0800

    todos los estados por SQLAlchemy

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -3,21 +3,23 @@
 Script que enumera todos los objetos de estado de la database hbtn_0e_6_usa
 """
 
-import sys
+from sys import argv
+from model_state import Base, State
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from model_state import State
+from sqlalchemy.orm import Session
 
-if _name_ == "_main_":
-
+if __name__ == "__main__":
     engine = create_engine(
-            "mysql+mysqldb://{}:{}@localhost/{}"
-            .format(sys.argv[1],
+            "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+            sys.argv[1],
             sys.argv[2],
             sys.argv[3]),
             pool_pre_ping=True)
-    Session = sessionmaker(bind=engine)
-    session = Session()
+    Base.metadata.create_all(engine)
 
-    for state in session.query(State).order_by(State.id):
+    session = Session(bind=engine)
+    states = session.query(State).order_by(State.id).all()
+
+    for state in states:
         print("{}: {}".format(state.id, state.name))
+        session.close()
