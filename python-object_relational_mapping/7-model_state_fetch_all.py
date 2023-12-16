5bd1b2a4f2aef34df19aa286ad906c2c5a72b6b6
SacariLuz <asacaricachi@gmail.com>
2023-12-15 21:30:57 -0800

    todos los estados por SQLAlchemy

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -16,7 +16,6 @@ if __name__ == "__main__":
             sys.argv[2],
             sys.argv[3]),
             pool_pre_ping=True)
-
     Session = sessionmaker(bind=engine)
     session = Session()
 
